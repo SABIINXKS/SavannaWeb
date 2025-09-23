32a322222ac83394846141899020d61812741a1a
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-23 19:00:33 +0300

    created GameController.cs. DB. Views/Game folder.

---
+++ SavannaWeb/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SavannaWeb.Controllers
+{
+    [Authorize]
+    public class GameController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public GameController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user.Id;
+
+
+            return View();
+        }
+    }
+}
