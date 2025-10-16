ef6d8123fbd1f2853cea63b4a39622de1ea84666
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-10-16 12:42:46 +0300

    added animal

--- SavannaWeb.Logic/SavannaGridHelper.cs
+++ SavannaWeb.Logic/SavannaGridHelper.cs
@@ -17,7 +17,8 @@ namespace SavannaWeb.Logic
             {
                 new Animal { Species = "Lion", X = 2, Y = 3 },
                 new Animal { Species = "Zebra", X = 5, Y = 7 },
-                new Animal { Species = "Elephant", X = 1, Y = 1 }
+                new Animal { Species = "Elephant", X = 1, Y = 1 },
+                new Animal { Species = "Giraffe", X = 8, Y = 4 }
             };
         }
 
